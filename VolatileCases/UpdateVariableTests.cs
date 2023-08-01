fd4d60e9573d795a6572606eeea5388a0f313efc
setu <sebastian.turek@ailleron.com>
2023-08-01 09:07:51 +0200

    Mofify

--- VolatileCases/UpdateVariableTests.cs
+++ VolatileCases/UpdateVariableTests.cs
@@ -3,13 +3,15 @@ using Xunit.Abstractions;
 namespace VolatileCases
 {
     /// <summary>
-    /// This is example of updating shared variable by multiple threads. 
-    /// Each test has two test cases. One testcase should update variable 100 times per thread.
-    /// The other 1000 times per thread. 
-    /// We can see that sometimes when variable is not marked as volatile 
-    /// There is error in assertion e.g. Should be 2000, but got 1998.
-    /// This is why volatile is so important. Without it we can get 
-    /// wrong state of variable which can lead to unpredictible application behavior.
+    /// This is example of updating and reading value from shared variable by multiple threads. 
+    /// One thread should update variable 100 and the other should read value 100 times.
+    /// Thanks to volatile we have correct state everytime reading the value.
+    /// 
+    /// Value is shared in memory between threads everytime when changed. 
+    /// Volatile is good when one thread only reads value and the othere only makes only writes.
+    /// It is not safe e.g. while incrementing value where read and write happens one after another,
+    /// because volatile does not prevent from out of order execution of processor which can 
+    /// change the order of executing writes and reads in multithreaded environment
     /// 
     /// Note from MSDN:
     /// On a multiprocessor system, a volatile read operation does not 
@@ -21,7 +23,6 @@ namespace VolatileCases
     public class UpdateVariableTests
     {
         private readonly ITestOutputHelper output;
-        private int sharedValue = 0;
         private volatile int sharedVolatileValue = 0;
 
         public UpdateVariableTests(ITestOutputHelper output)
@@ -29,72 +30,37 @@ namespace VolatileCases
             this.output = output;
         }
 
-        [Theory]
-        [InlineData(100, 200)]
-        [InlineData(1000, 2000)]
-        public void UpdateVariableRaceConditionTest(int iterates, int expectedResult)
+
+        [Fact]
+        public void UpdateVolatileVariableTest()
         {
-            Thread writer1Thread = new Thread(() =>
+            Thread writerThread = new Thread(() =>
             {
-                for (int i = 1; i <= iterates; i++)
+                for (int i = 1; i <= 100; i++)
                 {
-                    sharedValue++;
+                    output.WriteLine($"{DateTime.Now.ToString("HH:mm:ss:fffffff")} save: {i}");
+                    sharedVolatileValue = i;
+                    output.WriteLine($"{DateTime.Now.ToString("HH:mm:ss:fffffff")} done: {i}");
                     Thread.Sleep(100);
                 }
             });
 
-            Thread writer2Thread = new Thread(() =>
+            Thread readerThread = new Thread(() =>
             {
-                for (int i = 0; i < iterates; i++)
+                for (int i = 0; i < 100; i++)
                 {
-                    sharedValue++;
+                    output.WriteLine($"{DateTime.Now.ToString("HH:mm:ss:fffffff")} read: {sharedVolatileValue}");
                     Thread.Sleep(100);
                 }
             });
 
-            writer1Thread.Start();
-            writer2Thread.Start();
+            writerThread.Start();
+            readerThread.Start();
 
-            writer1Thread.Join();
-            writer2Thread.Join();
+            writerThread.Join();
+            readerThread.Join();
 
             output.WriteLine("Program completed.");
-            output.WriteLine($"Value result: {sharedValue}.");
-            Assert.Equal(expectedResult, sharedValue);
-        }
-
-        [Theory]
-        [InlineData(100, 200)]
-        [InlineData(1000, 2000)]
-        public void UpdateVolatileVariableTest(int iterates, int expectedResult)
-        {
-            Thread writer1Thread = new Thread(() =>
-            {
-                for (int i = 1; i <= iterates; i++)
-                {
-                    sharedVolatileValue++;
-                    Thread.Sleep(100);
-                }
-            });
-
-            Thread writer2Thread = new Thread(() =>
-            {
-                for (int i = 0; i < iterates; i++)
-                {
-                    sharedVolatileValue++;
-                    Thread.Sleep(100);
-                }
-            });
-
-            writer1Thread.Start();
-            writer2Thread.Start();
-
-            writer1Thread.Join();
-            writer2Thread.Join();
-
-            output.WriteLine("Program completed.");
-            output.WriteLine($"Value result: {sharedVolatileValue}.");
-            Assert.Equal(expectedResult, sharedVolatileValue);
         }
     }
 }
